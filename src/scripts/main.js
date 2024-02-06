81becf604b36b6c77783bf26c33692f1e434c2fd
Leonardo Abril <leoabril98@gmail.com>
2024-02-05 22:10:24 -0300

    criando a contagem regressiva

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,28 @@
-console.log("ola, mundo");
+AOS.init();
+
+const dataDoEvento = new Date("feb 12, 2024 19:00:00");
+const timeStampDoEvento =  dataDoEvento.getTime();
+
+const contaAsHoras = setInterval(function() {
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+
+    const distanciaAteOEvento = timeStampDoEvento - timeStampAtual;
+
+
+    const diaEmMS = 1000 * 60 * 60 * 24; 
+    const horaEmMS = 1000 * 60 * 60; 
+    const minutoEmMS = 1000 * 60; 
+
+    const diasAteOEvento =  Math.floor(distanciaAteOEvento / diaEmMS);
+    const horasAteOEvento = Math.floor((distanciaAteOEvento % diaEmMS ) /  horaEmMS);
+    const minutosAteOEvento = Math.floor((distanciaAteOEvento % horaEmMS) /  minutoEmMS);
+    const segundosAteOEvento = Math.floor((distanciaAteOEvento % minutoEmMS) /  (1000));
+
+    document.getElementById('contador').innerHTML = `${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundosAteOEvento}s`;
+
+    if (distanciaAteOEvento < 0) {
+        clearInterval(contaAsHoras);
+        document.getElementById('contador').innerHTML = `Evento expirado`;
+    }
+}, 1000)
